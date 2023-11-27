1fa9741e61c1ecf95bbec584733e007aae870798
K. Allagbe <kotchikpa.allagbe@gmail.com>
2023-11-27 14:53:20 -0500

    issue #2: tests for fuzzy search

--- app/finesse_data/__init__.py
+++ app/finesse_data/__init__.py
@@ -1,5 +1,4 @@
 import logging
-import re
 
 import requests
 from fuzzywuzzy import process
@@ -13,6 +12,14 @@ class EmptyQueryError(Exception):
     """Raised when the search query is empty."""
 
 
+def find_best_match(search_string, candidates, match_threshold):
+    best_match_result = process.extractOne(search_string, candidates)
+    if not best_match_result or best_match_result[1] < match_threshold:
+        logging.info(f"No close match found for search string: {search_string}")
+        return None
+    return best_match_result[0]
+
+
 def fetch_data(finesse_data_url, query, match_threshold):
     if not query:
         logging.error("Empty search query received")
@@ -23,15 +30,11 @@ def fetch_data(finesse_data_url, query, match_threshold):
         response.raise_for_status()
         files = response.json()
         file_map = {file["name"]: file for file in files}
-        best_match_result = process.extractOne(query, file_map.keys())
-        if not best_match_result or best_match_result[1] < match_threshold:
-            logging.info(f"No close match found for query: {query}")
-            return None
-        best_match, _ = best_match_result
-        matching_file = file_map[best_match]
-        results_response = requests.get(matching_file["download_url"])
-        results_response.raise_for_status()
-        return results_response.json()
+        if best_match := find_best_match(query, file_map.keys(), match_threshold):
+            matching_file = file_map[best_match]
+            results_response = requests.get(matching_file["download_url"])
+            results_response.raise_for_status()
+            return results_response.json()
     except requests.RequestException as e:
         logging.error(f"finesse-data fetch failed: {e}", exc_info=True)
         raise FinesseDataFetchException(f"finesse-data fetch failed: {e}") from e
--- tests/test_finesse_data_fetch.py
+++ tests/test_finesse_data_fetch.py
@@ -3,7 +3,12 @@ from unittest.mock import Mock, patch
 
 import requests
 
-from app.finesse_data import EmptyQueryError, FinesseDataFetchException, fetch_data
+from app.finesse_data import (
+    EmptyQueryError,
+    FinesseDataFetchException,
+    fetch_data,
+    find_best_match,
+)
 
 
 class TestFetchData(unittest.TestCase):
@@ -13,6 +18,12 @@ class TestFetchData(unittest.TestCase):
         self.files = [
             {"name": "file1.json", "download_url": "https://example.com/file1.json"},
             {"name": "file2.json", "download_url": "https://example.com/file2.json"},
+        ]
+        self.candidates = [
+            "Annual Financial Report",
+            "Project Proposal March",
+            "Client Contact Information",
+            "Product Catalog",
         ]
 
     @patch("app.finesse_data.requests.get")
@@ -40,6 +51,36 @@ class TestFetchData(unittest.TestCase):
         mock_get.side_effect = requests.RequestException()
         with self.assertRaises(FinesseDataFetchException):
             fetch_data(self.finesse_data_url, "a query", self.match_threshold)
+
+    def test_exact_match(self):
+        result = find_best_match(
+            "Annual Financial Report", self.candidates, self.match_threshold
+        )
+        self.assertEqual(result, "Annual Financial Report")
+
+    def test_misspelled_match(self):
+        result = find_best_match(
+            "Project Propsal March", self.candidates, self.match_threshold
+        )
+        self.assertEqual(result, "Project Proposal March")
+
+    def test_transposed_letters_match(self):
+        result = find_best_match(
+            "Cleint Contact Information", self.candidates, self.match_threshold
+        )
+        self.assertEqual(result, "Client Contact Information")
+
+    def test_extra_letter_match(self):
+        result = find_best_match(
+            "Product Catatalog", self.candidates, self.match_threshold
+        )
+        self.assertEqual(result, "Product Catalog")
+
+    def test_no_match_found(self):
+        result = find_best_match(
+            "Completely Unrelated String", self.candidates, self.match_threshold
+        )
+        self.assertIsNone(result)
 
 
 if __name__ == "__main__":
