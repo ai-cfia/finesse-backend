ba8c6e770de1866567d6df3a8580a47d2baf6d56
Ibrahim Kabir <117961703+HsKocTets@users.noreply.github.com>
2024-01-09 15:53:39 -0500

    issue #27: static file implemented

--- app/blueprints/swagger.py
+++ app/blueprints/swagger.py
@@ -1,7 +1,7 @@
 from flask_swagger_ui import get_swaggerui_blueprint
 
 SWAGGER_URL = '/api/docs'  # URL for exposing Swagger UI (without trailing '/')
-API_URL = 'http://petstore.swagger.io/v2/swagger.json'  # Our API url (can of course be a local resource)
+API_URL = 'app/static/swagger.json'  # Our API url (can of course be a local resource)
 
 swagger_blueprint = get_swaggerui_blueprint(
     SWAGGER_URL,  # Swagger UI static files will be mapped to '{SWAGGER_URL}/dist/'
